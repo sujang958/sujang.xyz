7186f7e89f5394cd1e57f0a98edf70a14ecd12c1
unknown <enter7377@naver.com>
2021-07-27 03:45:33 +0900

    feat(index.tsx): done skills

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,128 @@
-import { FC } from "react";
+import { Dispatch, FC, MouseEvent, SetStateAction, useState } from "react"
+import Link from "next/link"
+import Image from "next/image"
+
+const clickHandler = (isHidden: number, setIsHidden: Dispatch<SetStateAction<number>>, e: MouseEvent) => {
+  e.preventDefault()
+  if (isHidden == 1) {
+    setIsHidden(0)
+    document.querySelector('.skills')?.classList.remove('hidden')
+  } else {
+    setIsHidden(1)
+    document.querySelector('.skills')?.classList.add('hidden')
+  }
+}
 
 const Home: FC = () => {
+  const [isHidden, setIsHidden] = useState(1)
+
   return (
     <div className="text-left">
-      <div className="text-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 min-w-full p-14 shawdow-lg">
+      <div className="text-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 min-w-full p-14 shawdow-lg break-words">
         <p className="text-2xl md:text-4xl fade-in">Hello World_</p>
-        <p className="text-xs md:text-sm fade-in">sujang958</p>
+        <Link passHref={true} href="https://github.com/sujang958">
+          <p className="text-xs md:text-sm fade-in underline">
+            Visit Github
+          </p>
+        </Link>
       </div>
-      <div className="px-96 mx-96 mt-8 overflow-hidden">
-        <p className="text-2xl">Hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</p>
+      <div className="mt-4 md:mt-9 overflow-hidden mx-10 md:text-left md:text-center">
+        <div className="mt-1 break-words">
+          <p className="text-2xl">Hi!, I&#39;m Sujang958</p>
+          <p className="text-base">I&#39;m a student developer</p>
+        </div>
+        <div className="mt-8">
+          <p className="text-xl">Skills</p>
+          <p className="text-base mt-1 md:mt-2">
+            <p className="ctox text-xs underline" onClick={e => clickHandler(isHidden, setIsHidden, e)}>Click to see/hide</p>
+            <div className="mt-1.5 md:mt-2 skills hidden">
+              <Image 
+                src={"https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"}
+                alt="javascript"
+                width={112}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white"}
+                alt="javascript"
+                width={112}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white"}
+                alt="javascript"
+                width={79}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"}
+                alt="javascript"
+                width={79}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/python-%2314354C.svg?style=for-the-badge&logo=python&logoColor=white"}
+                alt="javascript"
+                width={89}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white"}
+                alt="javascript"
+                width={69}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/node.js-%2343853D.svg?style=for-the-badge&logo=node.js&logoColor=white"}
+                alt="javascript"
+                width={91}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB"}
+                alt="javascript"
+                width={111}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"}
+                alt="javascript"
+                width={79}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"}
+                alt="javascript"
+                width={130}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white"}
+                alt="javascript"
+                width={86}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white"}
+                alt="javascript"
+                width={74}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/nextjs-%23000000.svg?style=for-the-badge&logo=next.js&logoColor=white"}
+                alt="javascript"
+                width={88}
+                height={25}
+              />
+              <Image 
+                src={"https://img.shields.io/badge/Electron-191970?style=for-the-badge&logo=Electron&logoColor=white"}
+                alt="javascript"
+                width={92}
+                height={25}
+              />
+            </div>
+          </p>
+        </div>
       </div>
     </div>
   )
