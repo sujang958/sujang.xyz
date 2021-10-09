48c411d7df0459f6ea731cd04c2a0bccd0a7af34
unknown <enter7377@naver.com>
2021-10-10 01:44:39 +0900

    feat: added music page

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 
 const Footer: FC = () => {
     return (
-        <div className="px-8 pb-4 pt-8">
-            <hr />
-
+        <div className="px-8 pb-6">
+            <hr className="my-1" />
+            <p className="font-sans text-xs">built with Next.js</p>
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['img.shields.io']
+    domains: ['img.shields.io', 'img.icons8.com']
   }
 }
