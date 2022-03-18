13ab065bf2d07de76bac82561972d6314308f532
unknown <enter7377@naver.com>
2022-03-18 19:41:01 +0900

    fix: graphql uri

--- utils/graphql.ts
+++ utils/graphql.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 const client = new ApolloClient({
-  uri: "http://localhost:4015/api/graphql",
+  uri: "https://sujang.xyz/api/graphql",
   cache: new InMemoryCache(),
 })
 
