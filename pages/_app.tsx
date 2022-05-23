6b348d9c0b396e474021fc88cbbd92455bb7a6f4
unknown <enter7377@naver.com>
2022-05-23 21:02:34 +0900

    reaf: update modules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,61 +21,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         </div>
       </header>
       <main className="flex min-h-screen w-full flex-row pt-16">
-        <div className="flex w-1/4 flex-col items-start py-8">
-          <div className="flex w-full cursor-pointer flex-row items-center space-x-3 py-3 pl-6">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
-              />
-            </svg>
-            <p className="text-xl">Inbox</p>
-          </div>
-        </div>
-        <div className="pr-2 flex w-1/3 flex-col items-start py-8 rounded-r-xl">
-          <div className="flex cursor-pointer flex-col py-3 pl-6">
-            <div className="flex w-full flex-row items-center space-x-3 py-1">
-              <Avvvatars size={50} value="Seonghun Cho" />
-              <div className="flex flex-col">
-                <p className="text-xl">Seonghun Cho</p>
-                <p className="text-base leading-tight">
-                  {new Date().toISOString()}
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col py-1.5 px-1.5">
-              <p className="text-lg leading-tight text-gray-700">
-                hello there!, this is my introduce!. The concept is a mail
-                website.
-              </p>
-            </div>
-          </div>
-          <div className="flex cursor-pointer flex-col py-3 pl-6">
-            <div className="flex w-full flex-row items-center space-x-3 py-1">
-              <Avvvatars size={50} value="Seonghun Cho" />
-              <div className="flex flex-col">
-                <p className="text-xl">Seonghun Cho</p>
-                <p className="text-base leading-tight">
-                  {new Date().toISOString()}
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col py-1.5 px-1.5">
-              <p className="text-lg leading-tight text-gray-700">
-                This is my contact. If you have some questions, then you can ask
-                me.
-              </p>
-            </div>
-          </div>
-        </div>
+        <Component {...pageProps} />
       </main>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,65 @@
+import Avvvatars from "avvvatars-react"
 import { NextPage } from "next"
 
 const IndexPage: NextPage = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col items-center">
-      <p className="text-7xl">Ubuntu</p>
-    </div>
+    <>
+      <div className="flex w-1/4 flex-col items-start py-8">
+        <div className="flex w-full cursor-pointer flex-row items-center space-x-3 py-3 pl-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"
+            />
+          </svg>
+          <p className="text-xl">Inbox</p>
+        </div>
+      </div>
+      <div className="flex w-1/3 flex-col items-start rounded-r-xl py-8 pr-2">
+        <div className="flex cursor-pointer flex-col py-3 pl-6">
+          <div className="flex w-full flex-row items-center space-x-3 py-1">
+            <Avvvatars size={50} value="Seonghun Cho" />
+            <div className="flex flex-col">
+              <p className="text-xl">Seonghun Cho</p>
+              <p className="text-base leading-tight">
+                {new Date().toISOString()}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col py-1.5 px-1.5">
+            <p className="text-lg leading-tight text-gray-700">
+              hello there!, this is my introduce!. The concept is a mail
+              website.
+            </p>
+          </div>
+        </div>
+        <div className="flex cursor-pointer flex-col py-3 pl-6">
+          <div className="flex w-full flex-row items-center space-x-3 py-1">
+            <Avvvatars size={50} value="Seonghun Cho" />
+            <div className="flex flex-col">
+              <p className="text-xl">Seonghun Cho</p>
+              <p className="text-base leading-tight">
+                {new Date().toISOString()}
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col py-1.5 px-1.5">
+            <p className="text-lg leading-tight text-gray-700">
+              This is my contact. If you have some questions, then you can ask
+              me.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
