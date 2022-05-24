89e556eb61dd8fadb8f148cee4a91ae5f3d96282
unknown <enter7377@naver.com>
2022-05-24 22:17:01 +0900

    perf: add more responsive design

--- components/mailContainer.tsx
+++ components/mailContainer.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 
 const MailContainer: FC<{ children: any }> = ({ children }) => {
   return (
-    <div className="flex w-5/6 flex-col items-start rounded-r-xl py-8 pr-2 sm:w-[72%] md:w-[77%] lg:w-4/5 xl:w-5/6">
+    <div className="flex w-5/6 flex-col items-start rounded-r-xl py-8 pr-2 sm:w-[72%] sm:pl-3 md:w-[77%] md:pl-6 lg:w-4/5 xl:w-5/6">
       {children}
     </div>
   )
