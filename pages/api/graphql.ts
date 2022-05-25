c398c3c041551530f147f4cd26110441eb5aeb3d
unknown <enter7377@naver.com>
2022-05-25 19:17:03 +0900

    perf: now disable gql playground at production

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,12 +1,19 @@
 import { ApolloServer } from "apollo-server-cloud-functions"
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core/dist/plugin/landingPage/graphqlPlayground"
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled,
+} from "apollo-server-core"
 import typeDefs from "../../graphql/typeDefs"
 import resolvers from "../../graphql/resolvers"
 
 const server = new ApolloServer({
   resolvers,
   typeDefs,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  plugins: [
+    process.env.NODE_ENV === "production"
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+  ],
 })
 
 const handler = server.createHandler()
