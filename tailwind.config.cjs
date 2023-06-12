699a4405c7a82900ea5b1970b7a6960402f486a1
sujang.958 <enter7377@naver.com>
2023-06-12 21:04:42 +0900

    feat: add gradients on background

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,9 @@ module.exports = {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
     },
   },
   plugins: [],
