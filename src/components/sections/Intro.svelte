6c6dab59bd652fdc77e2a50c853363ab5d2a5120
sujang958 <sujangdev@gmail.com>
2024-08-09 00:18:15 +0900

    feat: implement button actions

--- src/components/sections/Intro.svelte
+++ src/components/sections/Intro.svelte
@@ -9,9 +9,12 @@
   ]
 
   let current = 0
+
+  $: if (current >= 3) current = 0
+  $: if (current <= 0) current = 0
 </script>
 
-<p class="text-5xl font-semibold leading-tight">
+<p class="text-5xl font-semibold leading-tight text-balance">
   {#each sentences as sentence, i}
     <span class={current !== i ? "text-neutral-600" : ""}>{sentence}&nbsp;</span
     >
@@ -21,14 +24,14 @@
 <div
   class="py-12 flex flex-row items-center justify-start self-start gap-x-2.5"
 >
-  <Button>
+  <Button on:click={() => (current -= 1)}>
     <MoveLeft class="h-5 w-5" />
     Read Previous
   </Button>
-  <Button class="p-2">
+  <Button on:click={() => (current = 0)} class="p-2">
     <RotateCcw class="h-4 w-4" />
   </Button>
-  <Button>
+  <Button on:click={() => (current += 1)}>
     Read Next
     <MoveRight class="h-5 w-5" />
   </Button>
