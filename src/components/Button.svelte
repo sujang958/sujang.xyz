30c4ec5b29cc0703a932b8c677b0eb268af63afa
sujang958 <sujangdev@gmail.com>
2024-08-08 23:30:53 +0900

    feat: add a hover color

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -5,7 +5,7 @@
 <button
   type="button"
   class={twMerge(
-    "flex flex-row items-center justify-center px-3 py-1.5 rounded-lg bg-neutral-900 text-sm text-white gap-x-2",
+    "flex flex-row items-center justify-center px-3 py-1.5 rounded-lg bg-neutral-900 text-sm text-white gap-x-2 transition duration-200 hover:bg-neutral-800",
     $$props.class,
   )}
 >
