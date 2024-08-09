9452fabf0556c465b0096fc4379ac61ab3a83a8a
sujang958 <sujangdev@gmail.com>
2024-08-09 23:48:44 +0900

    feat: add a simple footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from "astro/config"
 
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind"
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: { enabled: false },
   integrations: [tailwind({ applyBaseStyles: true }), svelte({})],
-});
+})
